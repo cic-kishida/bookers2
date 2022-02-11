b00f54629ab7f37e66df9d03e7bab3347bae94e4
EC2 Default User <ec2-user@ip-172-31-86-30.ec2.internal>
2022-02-11 03:04:56 +0000

    2022.02.11 基本機能の確認。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,10 @@ class BooksController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
     @user.user_id = current_user.id
     @user.save
     redirect_to books_path
-
   end
 
   def index
